942a2276ea7c44a1b0830c05a2634fb103cdea58
Sire65 <ha-joko@web.de>
2026-08-09 13:57:10 +0200

    Add verified Karriereleiter runtime manifest

---
+++ academy/karriereleiter/runtime/manifest.js
@@ -0,0 +1,7 @@
+'use strict';
+window.KC_KL_RUNTIME=Object.freeze({
+  version:'1.2.0',
+  format:'gzip-base64-chunks',
+  chunkCount:117,
+  sha256:'08b67fea8a00351ec18b2342486b7c16a83777183d238e1618ebfc2ffeb90961'
+});
